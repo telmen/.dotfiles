b701b6cfe9f8346e6e12149e1dd0aa9aa0ff06b4
telmen <flyinvoke@gmail.com>
2020-02-17 12:13:25 +0800

    add vim plug

--- common.sh
+++ common.sh
@@ -2,3 +2,7 @@
 
 # Rust
 curl https://sh.rustup.rs -sSf | sh
+
+# Vim Plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
